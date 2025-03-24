300ddf054c3fdcc6cdad996ae361d84f234b2cbb
uynvu078 <100883567+uynvu078@users.noreply.github.com>
2025-03-24 11:28:03 -0500

    Fixing routes for deployment

--- client/src/index.js
+++ client/src/index.js
@@ -2,5 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+const cors = require('cors');
+
+app.use(cors({
+    origin: 'https://uynvu078.github.io',
+    credentials: true,
+}));
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
