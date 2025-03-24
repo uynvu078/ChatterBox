b8f681186018ec8485fc52bd6ddfa2ea45c0a5b5
uynvu078 <100883567+uynvu078@users.noreply.github.com>
2025-03-24 11:30:11 -0500

    Connecting for deployment

--- server/index.js
+++ server/index.js
@@ -8,7 +8,11 @@ const PORT = process.env.PORT || 5000;
 
 require('dotenv').config();
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://uynvu078.github.io',
+    credentials: true,
+}));
+
 app.use(express.json());
 app.use(express.urlencoded());
 app.use('/auth', authRoutes);
