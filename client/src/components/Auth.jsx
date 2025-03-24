ab252e344a96af7b4094726467f88604edc96cb8
uynvu078 <100883567+uynvu078@users.noreply.github.com>
2025-03-24 11:20:25 -0500

    Connecting for deployment

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -29,7 +29,7 @@ const Auth = () => {
 
         const { username, password, phoneNumber, avatarURL } = form;
 
-        const URL = 'http://localhost:5000/auth';
+        const URL = `${process.env.REACT_APP_API_URL}/auth`;
 
         const { data: { token, userId, hashedPassword, fullName } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
             username, password, fullName: form.fullName, phoneNumber, avatarURL,
